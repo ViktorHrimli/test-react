0b3c448a3d9959aeb11442dbbfd3c8675d9d15f1
ViktorHrimli <viktorhrimli101@gmail.com>
2024-07-30 16:53:21 +0300

    fix bagl

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,13 @@
-import Image from "next/image";
-
-var url = process.env.URL;
-
 export default async function Home() {
-  var path = url ? url : "https://test-cloud-run-5ns7nwldjq-uc.a.run.app";
-  var res = await fetch(`${path}/api/photo?populate=*`);
+  var res = await fetch(`${process.env.URL}/api/tests/1?populate=*`);
 
   var photo = await res.json();
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Image
-        src={`${path}${photo.data.attributes.media.data.attributes.url}`}
-        w
-        fill
-      />
+      <h1 style={{ color: "red", fontSize: 40 }}>
+        {photo.data.attributes.dddd}
+      </h1>
     </div>
   );
 }
