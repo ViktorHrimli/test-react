db534ec2b5bae912351fa32d7138f6f8d561a48a
ViktorHrimli <viktorhrimli101@gmail.com>
2024-07-30 16:33:36 +0300

    fix bag

--- src/app/page.js
+++ src/app/page.js
@@ -3,16 +3,15 @@ import Image from "next/image";
 var url = process.env.URL;
 
 export default async function Home() {
-  var res = await fetch(`${url}/api/photo?populate=*`);
+  var path = url ? url : "https://test-cloud-run-5ns7nwldjq-uc.a.run.app";
+  var res = await fetch(`${path}/api/photo?populate=*`);
 
   var photo = await res.json();
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <Image
-        src={`${url ? url : "https://test-cloud-run-5ns7nwldjq-uc.a.run.app"}${
-          photo.data.attributes.media.data.attributes.url
-        }`}
+        src={`${path}${photo.data.attributes.media.data.attributes.url}`}
         w
         fill
       />
