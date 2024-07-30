6639a1eccea6789ba6e634ff84373e0a47a4cb71
ViktorHrimli <viktorhrimli101@gmail.com>
2024-07-30 16:20:55 +0300

    fix bag

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: "test-cloud-run-5ns7nwldjq-uc.a.run.app", protocol: "https" },
+    ],
+  },
+};
 
 export default nextConfig;
